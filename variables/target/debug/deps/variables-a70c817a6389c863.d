220b4f6c739374e6acddfc9f0f3d7f23c857f37a
ellieleep <32715340+ellieleep@users.noreply.github.com>
2022-06-23 00:31:25 -0400

    added variables project

---
+++ variables/target/debug/deps/variables-a70c817a6389c863.d
@@ -0,0 +1,5 @@
+/Users/ellieleep/IdeaProjects/rust_projects/variables/target/debug/deps/variables-a70c817a6389c863: src/main.rs
+
+/Users/ellieleep/IdeaProjects/rust_projects/variables/target/debug/deps/variables-a70c817a6389c863.d: src/main.rs
+
+src/main.rs:
