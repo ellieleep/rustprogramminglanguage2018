83583df96ee28c7787335d0ce4abac95d7bf135f
ellieleep <32715340+ellieleep@users.noreply.github.com>
2022-06-24 01:06:37 -0400

    Chapter 3 code examples

--- variables/target/debug/deps/variables-a70c817a6389c863.d
+++ variables/target/debug/deps/variables-a70c817a6389c863.d
@@ -1,5 +1,5 @@
-/Users/ellieleep/IdeaProjects/rust_projects/variables/target/debug/deps/variables-a70c817a6389c863: src/main.rs
+/Users/ellieleep/Documents/GitHub/rustprogramminglanguage2018/variables/target/debug/deps/variables-a70c817a6389c863: src/main.rs
 
-/Users/ellieleep/IdeaProjects/rust_projects/variables/target/debug/deps/variables-a70c817a6389c863.d: src/main.rs
+/Users/ellieleep/Documents/GitHub/rustprogramminglanguage2018/variables/target/debug/deps/variables-a70c817a6389c863.d: src/main.rs
 
 src/main.rs:
--- variables/target/debug/variables.d
+++ variables/target/debug/variables.d
@@ -1,1 +1,1 @@
-/Users/ellieleep/IdeaProjects/rust_projects/variables/target/debug/variables: /Users/ellieleep/IdeaProjects/rust_projects/variables/src/main.rs
+/Users/ellieleep/Documents/GitHub/rustprogramminglanguage2018/variables/target/debug/variables: /Users/ellieleep/Documents/GitHub/rustprogramminglanguage2018/variables/src/main.rs
